c4f225517e6bb29124c8c6ef2dee76df728da28d
kthanasi <konstantinos.thanasis@intrasoft-intl.com>
2022-01-23 21:41:34 +0200

    updated invoker

--- program-initializer/src/main/java/appinitializer/Invoker.java
+++ program-initializer/src/main/java/appinitializer/Invoker.java
@@ -1,19 +1,16 @@
 package appinitializer;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Invoker {
 
-    private Invoker(){}
+    private Invoker() {
+    }
 
     public static void invokeAll(List<Class<?>> classList) {
-
-        List<ClassWrapper<?>> classWrappers = classList.stream()
-                                                       .map(ClassWrapperCreator::createClassWrapper)
-                                                       .collect(Collectors.toList());
-
-        classWrappers.forEach(ClassWrapper::invokeAllMethods);
+        classList.stream()
+                 .map(ClassWrapperCreator::createClassWrapper)
+                 .forEach(ClassWrapper::invokeAllMethods);
     }
 
 
